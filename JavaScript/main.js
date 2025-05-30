5fa55e856ee0db370d34d49af0ac84f5664bdf96
Uzires <uziresportugallaureano@gmail.com>
2025-05-30 20:42:41 -0300

    adiciona as taxas de cambio e logica da converção de moe da

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -6,6 +6,22 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
     const valor = document.getElementById('amount').value;
     const daMoeda = document.getElementById('daMoeda').value;
     const  paraMoeda = document.getElementById('paraMoeda').value;
+// definir taxas de cambio
 
-    
-})
+const exenchangeRates ={
+    USD:{ BRL:5.73, EUR: 0.88 },
+    BRL:{ USD:0.17, EUR: 0.15 },
+    EUR:{ BRL:6.50, USD: 1.13 },
+};
+
+let valorConvertido;
+if (daMoeda === paraMoeda){
+    valorConvertido = valor;
+}else {
+    valorConvertido = valor * exenchangeRates[daMoeda][paraMoeda];
+}
+
+const convercao = document.getElementById("convercao");
+    convercao.textContent = `Resultado ${valorConvertido} ${paraMoeda}`
+;
+  }) ;
