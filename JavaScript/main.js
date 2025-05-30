c4d785aa7bcca1df729bca3279c9c3ba965877b5
Uzires <uziresportugallaureano@gmail.com>
2025-05-30 20:05:50 -0300

    adiona os primeiros codigos em java ja colocando as const para obter os valores

---
+++ JavaScript/main.js
@@ -0,0 +1,11 @@
+document.getElementById('currency-form').addEventListener('submit', function(event){
+    event.preventDefault();
+
+    // Obter valores de entrada
+
+    const valor = document.getElementById('amount').value;
+    const daMoeda = document.getElementById('daMoeda').value;
+    const  paraMoeda = document.getElementById('paraMoeda').value;
+
+    
+})
